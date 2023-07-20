1de8c810be264ebe0695e0fdbd544c16c85953ad
bengris32 <bengris32@protonmail.ch>
2023-07-20 14:56:18 +0100

    fleur: Build `android.hidl.allocator@1.0` vendor variant
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -55,7 +55,8 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     libhidltransport.vendor \
-    libhwbinder.vendor
+    libhwbinder.vendor \
+    android.hidl.allocator@1.0.vendor
 
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
