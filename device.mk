d1a304bc0e8a2ad34a5fde5b8abca6eafa0e3750
Mashopy <eliasgheeraert@gmail.com>
2023-08-24 21:22:16 -0400

    fleur: Build libsensorndkbridge

--- device.mk
+++ device.mk
@@ -384,6 +384,9 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@2.1.vendor \
     android.frameworks.sensorservice@1.0
 
+PRODUCT_PACKAGES += \
+    libsensorndkbridge
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
