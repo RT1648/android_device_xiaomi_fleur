280e0aade6317bebeb61cad94b9239cbb9612da1
Rohit Tiwari <rohitttiwari52@gmail.com>
2023-09-04 15:20:50 +0530

    fleur: Build gatekeeper impl/service from source
    Signed-off-by: Rohit Tiwari <rohitttiwari52@gmail.com>

--- device.mk
+++ device.mk
@@ -53,6 +53,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-service \
+    android.hardware.gatekeeper@1.0-impl
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
