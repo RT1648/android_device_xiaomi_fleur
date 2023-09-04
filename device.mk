790cdc252d3ba069fd9b577a83d002d3ff36d594
Rohit Tiwari <rohitttiwari52@gmail.com>
2023-09-04 17:26:01 +0530

    fleur: Build deafult render script
    Signed-off-by: Rohit Tiwari <rohitttiwari52@gmail.com>

--- device.mk
+++ device.mk
@@ -182,6 +182,10 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.config@1.3.vendor
 
+# Renderscript
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 # Wi-fi
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay
