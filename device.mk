609ae3963186fe2c9ccdf1305712abc5f9239a8f
Giovanni Ricca <giovanniricca@proton.me>
2023-12-19 15:56:52 +0000

    fleur: Build common iFAA service
    Change-Id: Ib777523cbb3cbc29086c8f6ef3cc0c31d49b9405

--- device.mk
+++ device.mk
@@ -196,6 +196,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/idc/uinput-fpc.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-fpc.idc \
     $(LOCAL_PATH)/configs/idc/uinput-goodix.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-goodix.idc
 
+# IFAA manager
+PRODUCT_PACKAGES += \
+    IFAAService
+
 PRODUCT_PACKAGES += \
     libshim_sink
 
