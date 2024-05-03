0a3f4b4aac907464f15c57998bcfd8bc60dd1538
bengris32 <bengris32@protonmail.ch>
2024-05-03 14:27:42 +0530

    fleur: Switch To MediaTek boot control HAL
    Change-Id: I4f453cfa2c44dcbbd7c611ec55624aff3d7e3d5d
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -14,8 +14,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl \
-    android.hardware.boot@1.2-impl.recovery \
+    android.hardware.boot@1.2-mtkimpl \
+    android.hardware.boot@1.2-mtkimpl.recovery \
     android.hardware.boot@1.2-service
 
 PRODUCT_PACKAGES += \
