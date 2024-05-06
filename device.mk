f7f3ade2778bd14d2331f39aafb8303ad840f81c
Rohit Tiwari <rohitttiwari52@gmail.com>
2024-05-06 22:01:12 +0530

    fleur: Configure Fingerprint Stack
    Signed-off-by: Rohit Tiwari <rohitttiwari52@gmail.com>

--- device.mk
+++ device.mk
@@ -139,6 +139,10 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.1-impl-mock \
     fastbootd
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health-service.mediatek \
