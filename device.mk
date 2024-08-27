40f831f62e6266baccc7be0c873a29aeeabcd0ae
Rohit Tiwari <rohitttiwari52@gmail.com>
2024-08-27 22:52:19 +0530

    fleur: Build libprotobuf-cpp-full-3.9.1-vendorcompat

--- device.mk
+++ device.mk
@@ -178,7 +178,8 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.2.vendor \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.4.vendor \
-    libprotobuf-cpp-lite-3.9.1-vendorcompat
+    libprotobuf-cpp-lite-3.9.1-vendorcompat \
+    libprotobuf-cpp-full-3.9.1-vendorcompat
 
 # Enable DM file pre-opting to reduce first boot time
 PRODUCT_DEX_PREOPT_GENERATE_DM_FILES := true
